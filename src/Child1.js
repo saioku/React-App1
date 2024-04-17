220e922429cf38e28c614b1ad6a5fbf2ef39c511
saioku <oku.saipranith@gmail.com>
2024-04-17 00:39:33 -0400

    all files

--- src/Child1.js
+++ src/Child1.js
@@ -27,12 +27,12 @@ class child1 extends Component {
             .attr("transform", 'translate(${margin.left}, ${margin.top})');
 
         var x_data = data.map(item => item.total_bill)
-        const x_scale = d3.selectLinear().domain([0, d3.max(x_data)]).range([margin.left, w]);
+        const x_scale = d3.scaleLinear().domain([0, d3.max(x_data)]).range([margin.left, w]);
         container.selectAll(".x_axis_g").data([0]).join('g').attr("class", 'x_axis_g')
             .attr("transform", 'translate(0, ${h})').call(d3.axisBottom(x_scale));
 
         var y_data = data.map(item => item.tip)
-        const y_scale = d3.selectLinear().domain([0, d3.max(y_data)]).range([h, 0]);
+        const y_scale = d3.scaleLinear().domain([0, d3.max(y_data)]).range([h, 0]);
         container.selectAll(".y_axis_g").data([0]).join('g').attr("class", "x_axis_g")
             .attr("transform", 'tanslate(${margin.left},0)').call(d3.axisLeft(y_scale));
 
